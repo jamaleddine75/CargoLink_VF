3a6fa51649832b10feb0ee7f2bdad6a5d9c415ac
jamal eddin haddad <labirjamal@gmail.com>
2026-07-11 14:35:11 +0100

    feat: initialize frontend project with dependencies and add SkeletonLoader export

--- frontend/generate-token.cjs
+++
@@ -1,8 +0,0 @@
-const jwt = require('jsonwebtoken');
-
-const token = jwt.sign(
-  { sub: 'driver@cargolink.ma' },
-  'CargoLinkDevSecretKey2026AtLeast32Chars!',
-  { expiresIn: '1h' }
-);
-console.log(token);
